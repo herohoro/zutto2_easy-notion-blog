c2c6a623fa1300d4948d807c92f1b008d4b434ad
herohoro <mtwd.office.25@gmail.com>
2024-03-24 19:49:36 +0900

    Delate gtagCompo_snsButtons

--- components/social-buttons.tsx
+++ components/social-buttons.tsx
@@ -11,67 +11,27 @@ import {
   TwitterShareButton,
 } from 'react-share'
 
-import * as gtag from '../lib/gtag'
 import styles from '../styles/social-buttons.module.css'
 
 const SocialButtons = ({ title = '', url, id = null }) => (
   <ul className={styles.socialButtons}>
     <li>
-      <TwitterShareButton
-        url={url}
-        title={title}
-        beforeOnClick={() =>
-          gtag.share({
-            method: 'twitter',
-            contentType: 'article',
-            itemId: id,
-          })
-        }
-      >
+      <TwitterShareButton url={url} title={title}>
         <TwitterIcon size={32} round={true} />
       </TwitterShareButton>
     </li>
     <li>
-      <FacebookShareButton
-        url={url}
-        beforeOnClick={() =>
-          gtag.share({
-            method: 'facebook',
-            contentType: 'article',
-            itemId: id,
-          })
-        }
-      >
+      <FacebookShareButton url={url}>
         <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
     </li>
     <li>
-      <PocketShareButton
-        url={url}
-        title={title}
-        beforeOnClick={() =>
-          gtag.share({
-            method: 'pocket',
-            contentType: 'article',
-            itemId: id,
-          })
-        }
-      >
+      <PocketShareButton url={url} title={title}>
         <PocketIcon size={32} round={true} />
       </PocketShareButton>
     </li>
     <li>
-      <HatenaShareButton
-        url={url}
-        title={title}
-        beforeOnClick={() =>
-          gtag.share({
-            method: 'hatena',
-            contentType: 'article',
-            itemId: id,
-          })
-        }
-      >
+      <HatenaShareButton url={url} title={title}>
         <HatenaIcon size={32} round={true} />
       </HatenaShareButton>
     </li>
