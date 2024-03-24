eec5267b3d93172b0c01b6d1101ae02b2b085954
herohoro <mtwd.office.25@gmail.com>
2024-03-24 20:27:57 +0900

    Event SNS button

--- components/social-buttons.tsx
+++ components/social-buttons.tsx
@@ -11,27 +11,67 @@ import {
   TwitterShareButton,
 } from 'react-share'
 
+import { sendGAEvent } from '@next/third-parties/google'
 import styles from '../styles/social-buttons.module.css'
 
 const SocialButtons = ({ title = '', url, id = null }) => (
   <ul className={styles.socialButtons}>
     <li>
-      <TwitterShareButton url={url} title={title}>
+      <TwitterShareButton
+        url={url}
+        title={title}
+        beforeOnClick={() =>
+          sendGAEvent({
+            method: 'twitter',
+            contentType: 'article',
+            itemId: id,
+          })
+        }
+      >
         <TwitterIcon size={32} round={true} />
       </TwitterShareButton>
     </li>
     <li>
-      <FacebookShareButton url={url}>
+      <FacebookShareButton
+        url={url}
+        beforeOnClick={() =>
+          sendGAEvent({
+            method: 'facebook',
+            contentType: 'article',
+            itemId: id,
+          })
+        }
+      >
         <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
     </li>
     <li>
-      <PocketShareButton url={url} title={title}>
+      <PocketShareButton
+        url={url}
+        title={title}
+        beforeOnClick={() =>
+          sendGAEvent({
+            method: 'pocket',
+            contentType: 'article',
+            itemId: id,
+          })
+        }
+      >
         <PocketIcon size={32} round={true} />
       </PocketShareButton>
     </li>
     <li>
-      <HatenaShareButton url={url} title={title}>
+      <HatenaShareButton
+        url={url}
+        title={title}
+        beforeOnClick={() =>
+          sendGAEvent({
+            method: 'hatena',
+            contentType: 'article',
+            itemId: id,
+          })
+        }
+      >
         <HatenaIcon size={32} round={true} />
       </HatenaShareButton>
     </li>
